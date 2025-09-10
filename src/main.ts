af086f42968a53a5a119e99b6a619e2c9a2d2203
Kraken0403 <trivedibhavya1997@gmail.com>
2025-09-10 14:21:12 +0530

    yes

--- src/main.ts
+++ src/main.ts
@@ -8,12 +8,13 @@ import * as path from 'path'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api/v1')
 
   app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
   // <-- ADD THIS BLOCK -->
   app.enableCors({
     // origin: 'http://localhost:3001',      // your Nuxt front-end
-    origin: '*',      // your Nuxt front-end
+    origin: 'http://195.35.21.6:5000',      // your Nuxt front-end
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type, Accept, Authorization',
     credentials: true,
