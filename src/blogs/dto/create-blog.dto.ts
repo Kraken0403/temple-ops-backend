06367b4f3a852bf9c5d6ba979d5ee39639d3cf6a
Kraken0403 <trivedibhavya1997@gmail.com>
2025-09-24 20:33:35 +0530

    'ess'

--- src/blogs/dto/create-blog.dto.ts
+++ src/blogs/dto/create-blog.dto.ts
@@ -1,7 +1,7 @@
 import { IsArray, IsBoolean, IsOptional, IsString, IsUrl } from 'class-validator';
 
 export class CreateBlogDto {
-  @IsString() title: string;
+  @IsString() title!: string;
   @IsOptional() @IsString() excerpt?: string;
   @IsOptional() @IsUrl() coverImageUrl?: string;
   @IsOptional() @IsString() bodyHtml?: string;
